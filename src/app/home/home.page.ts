7c2bca96737d6ecf1d1d7e6768fb392021c1e047
Adeesha Nimsara <kabigun@gmail.com>
2024-03-01 12:39:21 +0530

    Ion Fab button hide in settings component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,43 @@
 import { Component, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonRouterOutlet, IonTabs, IonIcon, IonTabBar, IonTabButton, IonFab, IonFabButton, IonFabList, IonInput, IonItem, IonButton, IonModal, IonButtons, IonList, IonItemSliding, IonItemOptions, IonItemOption, IonLabel, IonToast, IonFooter } from '@ionic/angular/standalone';
 import { AddTodoComponent } from "./add-todo/add-todo.component";
-
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonFooter, IonToast, IonLabel, IonItemOption, IonItemOptions, IonItemSliding, IonList, IonButtons, IonModal, IonButton, IonItem, IonInput, IonFabList, IonFabButton, IonFab, IonTabButton, IonTabBar, IonIcon, IonTabs, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, RouterOutlet, AddTodoComponent]
+  imports: [CommonModule, IonFooter, IonToast, IonLabel, IonItemOption, IonItemOptions, IonItemSliding, IonList, IonButtons, IonModal, IonButton, IonItem, IonInput, IonFabList, IonFabButton, IonFab, IonTabButton, IonTabBar, IonIcon, IonTabs, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, RouterOutlet, AddTodoComponent]
 })
 export class HomePage {
+  @ViewChild(IonModal)
+  modal!: IonModal;
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.checkRouter()
+  }
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkRouter();
+      }
+    });
+  }
+
+  name: string | undefined;
   tabName = 'ToDo'
   toastOpened = false
   toastMessage = ''
   toastColor = ''
+  isFabopen = true
+  isModalOpen = false;
 
   getTabName(name: string) {
     this.tabName = name
   }
-
-  @ViewChild(IonModal)
-  modal!: IonModal;
-
-  name: string | undefined;
 
   hideModal() {
     this.modal.dismiss(this.name, 'confirm');
@@ -36,5 +47,18 @@ export class HomePage {
     this.toastOpened = false
     this.toastMessage = ''
     this.toastColor = ''
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  checkRouter() {
+    const currentRoute = this.router.url
+    if (currentRoute === '/settings') {
+      this.isFabopen = false
+    } else {
+      this.isFabopen = true
+    }
   }
 }
--- src/app/home/settings/settings.component.ts
+++ src/app/home/settings/settings.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonToggle, IonList, IonNote, IonTextarea, IonItemSliding, IonItemOptions, IonItemOption, IonLabel, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonRouterOutlet, IonContent, IonInput, IonButton, IonIcon } from '@ionic/angular/standalone'
+import { IonToggle, IonList, IonNote, IonTextarea, IonItemSliding, IonItemOptions, IonItemOption, IonLabel, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonRouterOutlet, IonContent, IonInput, IonButton, IonIcon, IonText } from '@ionic/angular/standalone'
 import { AuthService } from 'src/app/services/user/auth.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
   standalone: true,
-  imports: [IonIcon, IonButton, IonContent, IonList, IonItem, IonInput, IonToggle, IonLabel, IonNote, IonTextarea]
+  imports: [IonText, IonIcon, IonButton, IonContent, IonList, IonItem, IonInput, IonToggle, IonLabel, IonNote, IonTextarea]
 })
 export class SettingsComponent implements OnInit {
+
+  userName = this.auth.getUser()?.displayName
+  userEmail = this.auth.getUser()?.email
 
   constructor(private auth:AuthService) { }
 
@@ -18,5 +21,7 @@ export class SettingsComponent implements OnInit {
   logOut(){
     this.auth.signOut()
   }
+
+
 
 }
